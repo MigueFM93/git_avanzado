09d2f306076ca44ee14e99cc536bdcf97c18819d
FJ <forgex18@gmail.com>
2017-05-11 16:41:50 +0200

    Generando números aleatorios para las bolas blancas y la power ball

--- pb.c
+++ pb.c
@@ -43,6 +43,18 @@ int calculate_result(int white_balls[5], int power_ball)
     return 0;
 }
 
+int whiteballs_computer_generated() 
+{ 
+	return rand()%59+1; 
+} 
+
+int powerball_computer_generated() 
+{ 
+	return rand()%39+1; 
+} 
+
+
+
 int main(int argc, char** argv)
 {
 	int balls[6];
@@ -77,6 +89,23 @@ int main(int argc, char** argv)
  		result = result * 2;
  	}
 
+ 	    if (6 != count_balls) 
+	    {
+	      for (int i = 0; i < 5; i++){ 
+	   			balls[i] = whiteballs_computer_generated(); 
+	      }
+	balls[5] = powerball_computer_generated(); // Power ball 
+
+	      printf("Your numbers are: "); 
+			for (int i = 0; i < 5; i++){ 
+			   printf("%d ", balls[i]); 
+			      }
+			      printf("\
+			nAnd the power ball:"); 
+			printf(" %d\
+			n", balls[5]); 
+			    }
+	
  	printf("%d percent chance of winning\n", result);
  	
  	return 0;
